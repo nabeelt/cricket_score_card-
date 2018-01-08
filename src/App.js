edbb6a85074025e358ce564ad24f4af113058125
nabeel <nabeel@users-iMac-5.local>
2018-01-08 13:12:29 +0530

    template added

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Crick live</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main-container />
+        <footer className="App-footer">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Crick live</h1>
+          <div className="bottom-sec">
+          &copy; 2018 Cricklive. All rights reserved.
+          </div>
+        </footer>
       </div>
     );
   }
