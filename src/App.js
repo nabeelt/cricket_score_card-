484efa5054ed411d4120257db00bf44af7683027
Praveen-V-S <praveens@qburst.com>
2018-01-16 12:48:38 +0530

    conflict resolved

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 // import App from './App.js';
 import './index.css';
-import WagonContainer from './containers/wagonWheelContainer'
+import './styles/common/common.css';
+import wagonContainer from './containers/wagonWheelContainer'
+import createMatchContainer from './containers/createMatchContainer'
 
 export default () => (
   <Switch>
-    <Route path='/' component={WagonContainer} />
+    <Route exact path='/' component={wagonContainer} />
+    <Route exact path='/creatematch' component={createMatchContainer} />
   </Switch>
 )
--- src/Template.js
+++ src/Template.js
@@ -9,14 +9,8 @@ class Template extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" onClick={()=>this.props.history.push("")}/>
           <h1 className="App-title">Crick live</h1>
-          <button type="button" className="btn-login button" name="login" onClick={()=>this.props.history.push('/login')}>
-              <span>LOGIN</span>
-          </button>
-          <button type="button" className="btn-login button" name="signup" onClick={()=>this.props.history.push('/signup')}>
-              <span>SIGN UP</span>
-          </button>
         </header>
-         <div>{this.props.children?this.props.children:<h1>NO Layout specified</h1>}</div>
+         <div className="content">{this.props.children?this.props.children:<h1>NO Layout specified</h1>}</div>
         <footer className="App-footer">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Crick live</h1>
